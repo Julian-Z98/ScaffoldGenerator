58f327bd1733ed029f298487c35fd0d542949de1
Julian Zander <zanderjulian@gmx.de>
2021-11-02 16:53:26 +0100

    Performance test

---
+++ ScaffoldGenerator/SGPerformanceTest/src/Main.java
@@ -0,0 +1,47 @@
+/**
+ * Performance test for
+ * ScaffoldGenerator for CDK
+ * Copyright (C) 2021 Julian Zander
+ *
+ * Source code is available at <https://github.com/Julian-Z98/ScaffoldGenerator>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//package de.unijena.cheminf.performancetest;
+
+/**
+ * Main class for starting application.
+ *
+ * @author Julian Zander(zanderjulian@gmx.de)
+ */
+public class Main {
+
+    /**
+     * Starts the application. Command line arguments must be the name of an SD-file to read (must be located in the
+     * same directory as the application's .jar file) and the number of different threads to use for calculation.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            PerformanceTest tmpApplication = new PerformanceTest("ScaffoldGenerator\\src\\test\\resources\\PerformanceTest.sdf");
+            //PerformanceTest tmpApplication = new PerformanceTest("ScaffoldGenerator\\src\\test\\resources\\COCONUT_DB.sdf");
+        } catch (Exception anException) {
+            anException.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+
+}
---
+++ ScaffoldGenerator/SGPerformanceTest/src/PerformanceTest.java
@@ -0,0 +1,199 @@
+/**
+ * Performance test for
+ * ScaffoldGenerator for CDK
+ * Copyright (C) 2021 Julian Zander
+ *
+ * Source code is available at <https://github.com/Julian-Z98/ScaffoldGenerator>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//package de.unijena.cheminf.performancetest;
+
+import de.unijena.cheminf.scaffolds.ScaffoldGenerator;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.iterator.IteratingSDFReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * An application for testing the performance of the ScaffoldGenerator methods.
+ *
+ * @author Julian Zander(zanderjulian@gmx.de)
+ */
+public class PerformanceTest {
+
+    //<editor-fold defaultstate="collapsed" desc="Private static final constants">
+    /**
+     * Name of file for logging occurred exceptions
+     */
+    private static final String EXCEPTIONS_LOG_FILE_NAME = "Exceptions_Log.txt";
+
+    /**
+     * Name of file for writing results
+     */
+    private static final String RESULTS_FILE_NAME = "Results.txt";
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Private class variables">
+    /**
+     * The working directory (the jar-file's directory)
+     */
+    private String workingPath;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constructor">
+    /**
+     * Instantiates and starts the application. It first loads all molecules from a given SD file into memory and then
+     * distributes them equally on the given number of different threads to use. It measures the time it takes for all
+     * threads to complete the extraction of functional groups using the ScaffoldGenerator. It exits the system
+     * if an unexpected exception occurs that prevents the application from working, e.g. an IllegalArgumentException
+     * (will be logged to a file, not printed on the console).
+     *
+     * @param anArgs the command line arguments, anArgs[0] must be the name of the SD file to load (must be located in
+     * the same directory as the application's JAR file) and anArgs[1] must be the number of different threads to use
+     * @throws IOException if the constructor is unable to open a text file for logging occurred exceptions
+     */
+    public PerformanceTest(String anArgs) throws IOException {
+        /*Set up exception log file*/
+        this.workingPath = (new File("").getAbsoluteFile().getAbsolutePath()) + File.separator;
+        LocalDateTime tmpDateTime = LocalDateTime.now();
+        String tmpTimeStamp = tmpDateTime.format(DateTimeFormatter.ofPattern("uuuu_MM_dd_HH_mm"));
+        File tmpExceptionsLogFile = new File(this.workingPath
+                + PerformanceTest.EXCEPTIONS_LOG_FILE_NAME);
+        FileWriter tmpExceptionsLogFileWriter = new FileWriter(tmpExceptionsLogFile, true);
+        PrintWriter tmpExceptionsPrintWriter = new PrintWriter(tmpExceptionsLogFileWriter);
+        tmpExceptionsPrintWriter.println("#########################################################################");
+        tmpExceptionsPrintWriter.println("Time-stamp: " + tmpTimeStamp);
+        tmpExceptionsPrintWriter.println();
+        tmpExceptionsPrintWriter.flush();
+        tmpExceptionsPrintWriter.close();
+        try {
+            /*Load SD file*/
+            File tmpDBFile = new File(this.workingPath + anArgs);
+            FileInputStream tmpDBFileInputStream;
+            try {
+                tmpDBFileInputStream = new FileInputStream(tmpDBFile);
+            } catch (FileNotFoundException | SecurityException anException) {
+                throw new IllegalArgumentException("The database file (name) is not valid: " + anException.getMessage());
+            }
+
+            File tmpResultsLogFile = new File(this.workingPath
+                    + PerformanceTest.RESULTS_FILE_NAME);
+            FileWriter tmpResultsLogFileWriter = new FileWriter(tmpResultsLogFile, true);
+            PrintWriter tmpResultsPrintWriter = new PrintWriter(tmpResultsLogFileWriter);
+            tmpResultsPrintWriter.println("#########################################################################");
+            tmpResultsPrintWriter.println("Time-stamp: " + tmpTimeStamp);
+            tmpResultsPrintWriter.println();
+            tmpResultsPrintWriter.println("Application initialized. Loading database file named " + anArgs + ".");
+            tmpResultsPrintWriter.flush();
+            System.out.println("\nApplication initialized. Loading database file named " + anArgs + ".");
+            IteratingSDFReader tmpDBReader = new IteratingSDFReader(tmpDBFileInputStream, DefaultChemObjectBuilder.getInstance(), true);
+            /*Load molecules*/
+            List<IAtomContainer> tmpMoleculesList = new LinkedList<>();
+            while (tmpDBReader.hasNext()) {
+                try {
+                    IAtomContainer tmpMolecule = (IAtomContainer) tmpDBReader.next();
+                    tmpMoleculesList.add(tmpMolecule);
+                } catch (Exception anException) {
+                    /*If an IllegalArgumentException is thrown in applyFiltersAndPreprocessing (meaning that the molecule
+                    should be filtered) the molecule is skipped by catching this exception*/
+                }
+            }
+            try {
+                tmpDBReader.close();
+            } catch (IOException ex) {
+                this.appendToLogfile(ex);
+            }
+            tmpResultsPrintWriter.println("\nDone Loading database. Found and processed " + tmpMoleculesList.size() + " valid molecules.");
+            System.out.println("Done Loading database. Found and processed " + tmpMoleculesList.size() + " valid molecules.");
+            /*Generate 10 random subsets from 1/10 to 10/10 and generate a network and a forest out of them*/
+            int tmpListSize = tmpMoleculesList.size();
+            for (int tmpDeci = 1; tmpDeci < 10; tmpDeci++) {
+                int tmpRate = tmpListSize / 10 * tmpDeci;
+                tmpResultsPrintWriter.println("\nProcess " + tmpRate + " valid molecules.");
+                System.out.println("Process " + tmpRate + " valid molecules.");
+                long tmpSeed = System.nanoTime();
+                Collections.shuffle(tmpMoleculesList, new Random(tmpSeed));
+                List<IAtomContainer> tmpMoleculeSubList = tmpMoleculesList.subList(0, tmpRate);
+                tmpResultsPrintWriter.flush();
+                ScaffoldGenerator tmpScaffoldGenerator = new ScaffoldGenerator();
+                long tmpStartTime = System.currentTimeMillis();
+                /*Real measured process*/
+                tmpScaffoldGenerator.generateScaffoldNetwork(tmpMoleculeSubList);
+                long tmpEndTime = System.currentTimeMillis();
+                tmpResultsPrintWriter.println("Network generation took " + (tmpEndTime - tmpStartTime) + " ms.");
+                System.out.println("Network generation took " + (tmpEndTime - tmpStartTime) + " ms.");
+                tmpStartTime = System.currentTimeMillis();
+                /*Real measured process*/
+                tmpScaffoldGenerator.generateSchuffenhauerForest(tmpMoleculeSubList);
+                tmpEndTime = System.currentTimeMillis();
+                tmpResultsPrintWriter.println("Forest generation took " + (tmpEndTime - tmpStartTime) + " ms.");
+                System.out.println("Forest generation took " + (tmpEndTime - tmpStartTime) + " ms.");
+            }
+            tmpResultsPrintWriter.flush();
+            tmpResultsPrintWriter.println();
+            tmpResultsPrintWriter.flush();
+            tmpResultsPrintWriter.close();
+        } catch (Exception anException) {
+            this.appendToLogfile(anException);
+            anException.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+    //</editor-fold>
+
+    /**
+     * Appends the given exception's stack trace to a log file.
+     *
+     * @param anException the exception to log
+     */
+    private void appendToLogfile(Exception anException) {
+        if (anException == null) {
+            return;
+        }
+        PrintWriter tmpPrintWriter = null;
+        try {
+            FileWriter tmpFileWriter = new FileWriter(this.workingPath
+                    + PerformanceTest.EXCEPTIONS_LOG_FILE_NAME,
+                    true);
+            tmpPrintWriter = new PrintWriter(tmpFileWriter);
+            StringWriter tmpStringWriter = new StringWriter();
+            anException.printStackTrace(new PrintWriter(tmpStringWriter));
+            String tmpStackTrace = tmpStringWriter.toString();
+            tmpPrintWriter.println(tmpStackTrace);
+        } catch (IOException anIOException) {
+            anIOException.printStackTrace(System.err);
+        } finally {
+            if (tmpPrintWriter != null) {
+                tmpPrintWriter.println();
+                tmpPrintWriter.flush();
+                tmpPrintWriter.close();
+            }
+        }
+    }
+}
