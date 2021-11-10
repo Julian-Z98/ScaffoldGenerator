df35e0f1d85fe1a4033725d0a36bce4279d8c5e4
Julian Zander <zanderjulian@gmx.de>
2021-11-10 12:22:31 +0100

    JavaDoc update

--- ScaffoldGenerator/src/main/java/de/unijena/cheminf/scaffolds/performanceTest/Main.java
+++ ScaffoldGenerator/src/main/java/de/unijena/cheminf/scaffolds/performanceTest/Main.java
@@ -1,4 +1,4 @@
-package de.unijena.cheminf.scaffolds.performanceTest; /**
+ /**
  * Performance test for
  * ScaffoldGenerator for CDK
  * Copyright (C) 2021 Julian Zander
@@ -19,10 +19,10 @@ package de.unijena.cheminf.scaffolds.performanceTest; /**
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//package de.unijena.cheminf.performancetest;
+package de.unijena.cheminf.scaffolds.performanceTest;
 
 /**
- * de.unijena.cheminf.scaffolds.performanceTest.Main class for starting application.
+ * Main class for starting the PerformanceTest application.
  *
  * @author Julian Zander(zanderjulian@gmx.de)
  */
@@ -34,11 +34,11 @@ public class Main {
      *
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String args) {
         try {
-            //de.unijena.cheminf.scaffolds.performanceTest.PerformanceTest tmpApplication = new de.unijena.cheminf.scaffolds.performanceTest.PerformanceTest("ScaffoldGenerator\\src\\test\\resources\\de.unijena.cheminf.scaffolds.performanceTest.PerformanceTest.sdf");
-            //de.unijena.cheminf.scaffolds.performanceTest.PerformanceTest tmpApplication = new de.unijena.cheminf.scaffolds.performanceTest.PerformanceTest(args);
-            PerformanceTest tmpApplication = new PerformanceTest("ScaffoldGenerator\\src\\test\\resources\\COCONUT_DB1000.sdf");
+            //PerformanceTest tmpApplication = new PerformanceTest("ScaffoldGenerator\\src\\test\\resources\\PerformanceTest.sdf");
+            //PerformanceTest tmpApplication = new PerformanceTest("ScaffoldGenerator\\src\\test\\resources\\COCONUT_DB1000.sdf");
+            PerformanceTest tmpApplication = new PerformanceTest(args);
         } catch (Exception anException) {
             anException.printStackTrace(System.err);
             System.exit(1);
